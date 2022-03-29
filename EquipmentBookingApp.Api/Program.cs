97176cf8c27d7a3c6ba14e9bb41698bb7c3f05fd
Trevoir Williams <trevoir.williams@gmail.com>
2022-03-28 20:32:09 -0500

    Registere Processors in Program.cs

--- EquipmentBookingApp.Api/Program.cs
+++ EquipmentBookingApp.Api/Program.cs
@@ -1,3 +1,5 @@
+using EquipmentBookingApp.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IEquipmentBookingProcessor, EquipmentBookingProcessor>();
+
 
 var app = builder.Build();
 
